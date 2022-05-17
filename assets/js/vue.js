2790c3fde570192753943791fc23d7399384d2ec
Morlet <34801852+Reptilz@users.noreply.github.com>
2022-05-17 11:15:39 +0200

    Ajout du Routeur

---
+++ assets/js/vue.js
@@ -0,0 +1,28 @@
+//component Home
+const Home = {
+  template: "<h1>Home</h1>",
+  name: "Home",
+};
+
+//component UserSettings
+const UserSettings = {
+  template: "<h1>UserSettings</h1>",
+  name: "Home",
+};
+
+//component WishList
+const WishList = {
+  template: "<h1>WishList</h1>",
+  name: "Home",
+};
+
+//component ShoppingCart
+const ShoppingCart = {
+  template: "<h1>ShoppingCart</h1>",
+  name: "Home",
+};
+
+//Call de VueJS
+const vue = new Vue({
+  router,
+}).$mount("#app");
